3a8b9a0e891fc8be8f77486100dac0500d765cbe
Vandal-William <vandal.william@hotmail.com>
2023-05-17 10:39:27 +0200

    finish create and style oneContactPage

--- client-ssr/src/components/Admin/oneContactPage/index.js
+++ client-ssr/src/components/Admin/oneContactPage/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
- import { connect, useDispatch, useSelector} from 'react-redux';
+import { connect, useDispatch, useSelector} from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 
 import NavAdmin from '../NavAdmin';
 import {updateContact} from '../../../reducers/contact';
+import './styles.scss'
 
 function OneContactPage() {
   const contact = useSelector(state => state.contact);
@@ -48,9 +49,8 @@ function OneContactPage() {
         <h2 className='onecontact-name'>{contact.firstname} - {contact.lastname}</h2>
         <form  className='onecontact-form'>
 
+          <h3 className='onecontact-legend'>Information de contact</h3>
           <fieldset className='onecontact-field'>
-
-            <legend className='onecontact-legend'>Information de contact</legend>
 
             <label className='onecontact-label' for="firstname">Nom de famille</label>
             <input onChange={handleChange}  className='onecontact-input' id="firstname" type="text" name="firstname" value={contact.firstname}/>
@@ -65,17 +65,17 @@ function OneContactPage() {
             <input onChange={handleChange} className='onecontact-input' id="phone" type="text" name="phone" value={contact.phone}/>
   
             {contact.re_contact && ( 
-              <> 
+              <div> 
                 <label className='onecontact-label' for="contact">Contact</label>
                 <input onChange={handleChange} className='onecontact-input' type="checkbox" name="contact" id="contact" value={contact.re_contact} checked/>
-              </>
+              </div>
             )}
   
             {!contact.re_contact && ( 
-              <> 
+              <div> 
                 <label className='onecontact-label' for="contact">Contact</label>
                 <input onChange={handleChange} className='onecontact-input' type="checkbox" name="contact" id="contact" value={contact.re_contact}/>
-              </>
+              </div>
             )}
   
             <label className='onecontact-label' for="status">Status</label>
@@ -90,8 +90,8 @@ function OneContactPage() {
 
           </fieldset>
 
+          <h3 className='onecontact-legend'>Entreprise</h3>
           <fieldset className='onecontact-field'>
-            <legend className='onecontact-legend'>Entreprise</legend>
 
             <label className='onecontact-label' for="company">Entreprise</label>
             <input onChange={handleChange} className='onecontact-input' id="company" type="text" name="company" value={contact.company}/>
@@ -104,38 +104,44 @@ function OneContactPage() {
 
             <label className='onecontact-label' for="company_city">Ville</label>
             <input onChange={handleChange} className='onecontact-input' id="company_city" type="text" name="company_city" value={contact.company_city}/>
-            <button onClick={handleSubmit}> Modifier</button>
+            
+            <button className='onecontact-submit' onClick={handleSubmit}> Modifier</button>
+
           </fieldset>
 
         </form>
+
+        <h3 className='onecontact-legend'>Problématiques</h3>
+
         <fieldset className='onecontact-field'>
-          <legend className='onecontact-legend'>Problématiques</legend>
           {contact.responses !== undefined ? contact.responses.map(response => (
-            <p key={response}>{response}</p>
+            <p className='onecontact-response' key={response}>{response}</p>
           )): 'loading...'}
         </fieldset>
+
+        <h3 className='onecontact-legend'>Rendez-vous</h3>
+
         <fieldset className='onecontact-field'>
 
-            <legend className='onecontact-legend'>Rendez-vous</legend>
-              <button>Ajouter un rendez-vous</button>
-              <table style={{width: '100%'}}>
-                  <thead>
+              <button className='onecontact-btn'>Ajouter un rendez-vous</button>
+              <table className='onecontact-table'>
+                  <thead className='onecontact-thead'>
                     <tr>
-                      <th style={{textAlign: 'start'}}>Date</th>
-                      <th style={{textAlign: 'end'}}>Heure</th>
-                      <th style={{textAlign: 'end'}}>Commentaire</th>
-                      <th style={{textAlign: 'end'}}>Action</th>
+                      <th className='onecontact-th' style={{textAlign: 'start'}}>Date</th>
+                      <th className='onecontact-th' style={{textAlign: 'end'}}>Heure</th>
+                      <th className='onecontact-th' style={{textAlign: 'end'}}>Commentaire</th>
+                      <th className='onecontact-th' style={{textAlign: 'end'}}>Action</th>
                     </tr>
                   </thead>
-                  <tbody>
+                  <tbody className='onecontact-tbody'>
                   {contact.meets !== undefined ? contact.meets.map(meet => {
                       return (
-                        <tr>
+                        <tr className='onecontact-active'>
                           <>
-                            <td style={{textAlign: 'start'}}>{meet.date}</td>
-                            <td style={{textAlign: 'end'}}>{meet.time}</td>
-                            <td style={{textAlign: 'end'}}>{meet.subject}</td>
-                            <td style={{textAlign: 'end'}}> <a href="#">Modifier</a> </td>
+                            <td className='onecontact-td' style={{textAlign: 'start'}}>{meet.date}</td>
+                            <td className='onecontact-td' style={{textAlign: 'end'}}>{meet.time}</td>
+                            <td className='onecontact-td' style={{textAlign: 'end'}}>{meet.subject}</td>
+                            <td className='onecontact-td' style={{textAlign: 'end'}}> <a href="#">Modifier</a> </td>
 
                           </>
                         </tr>
@@ -144,39 +150,41 @@ function OneContactPage() {
                   </tbody>
                 </table>
           </fieldset>
+
+        <h3 className='onecontact-legend'>Devis</h3>
+
         <fieldset className='onecontact-field'>
-          <legend className='onecontact-legend'>Devis</legend>
-          <button>Créer un devis</button>
+          <button className='onecontact-btn'>Créer un devis</button>
           {contact.training_quotation !== undefined ? contact.training_quotation.map(bill => {
             let total = 0; // initialise la variable pour stocker la somme
             return (
               <div key={bill.quotation}>
-                <div>
+                <div className='onecontact-cta'>
                     <h3>Devis N° {bill.quotation}</h3>
-                    <button>Imprimer le devis</button>
-                    <button>Modifier le devis</button>
-                    <button>Supprimer le devis</button>
+                    <button className='onecontact-btn'>Imprimer le devis</button>
+                    <button className='onecontact-btn'>Modifier le devis</button>
+                    <button className='onecontact-btn'>Supprimer le devis</button>
                 </div>
-                <table style={{width: '100%'}}>
-                  <thead>
+                <table className='onecontact-table'>
+                  <thead className='onecontact-thead'>
                     <tr>
-                      <th style={{textAlign: 'start'}}>Formations</th>
-                      <th style={{textAlign: 'end'}}>Durées</th>
-                      <th style={{textAlign: 'end'}}>prix / h</th>
-                      <th style={{textAlign: 'end'}}>Total</th>
+                      <th className='onecontact-th' style={{textAlign: 'start'}}>Formations</th>
+                      <th className='onecontact-th' style={{textAlign: 'end'}}>Durées</th>
+                      <th className='onecontact-th' style={{textAlign: 'end'}}>prix / h</th>
+                      <th className='onecontact-th' style={{textAlign: 'end'}}>Total</th>
                     </tr>
                   </thead>
-                  <tbody>
+                  <tbody className='onecontact-tbody'>
                     {bill.trainings.map(training => {
                       const totalTraining = training.price * training.duration;
                       total += totalTraining; // met à jour la somme
                       return (
                         <tr key={training.name}>
                           <>
-                            <td>{training.name}</td>
-                            <td style={{textAlign: 'end'}}>{training.duration}h</td>
-                            <td style={{textAlign: 'end'}}>{training.price}€</td>
-                            <td style={{textAlign: 'end'}}>{totalTraining}€</td>
+                            <td className='onecontact-td'>{training.name}</td>
+                            <td className='onecontact-td' style={{textAlign: 'end'}}>{training.duration}h</td>
+                            <td className='onecontact-td' style={{textAlign: 'end'}}>{training.price}€</td>
+                            <td className='onecontact-td' style={{textAlign: 'end'}}>{totalTraining}€</td>
                           </>
                         </tr>
                       )
@@ -184,7 +192,7 @@ function OneContactPage() {
                   </tbody>
                   <tfoot>
                     <tr>
-                      <td colSpan={4}  style={{textAlign: 'end', color : 'green'}}>Total : {total}€</td>
+                      <td className='onecontact-td' colSpan={4}  style={{textAlign: 'end', color : 'green'}}>Total : {total}€</td>
                     </tr>
                   </tfoot>
                 </table>
@@ -193,34 +201,34 @@ function OneContactPage() {
           }) : 'loading...'}
         </fieldset>
 
+        <h3 className='onecontact-legend'>Contrats</h3>
+
         <fieldset className='onecontact-field'>
-          <legend className='onecontact-legend'>Contrats</legend>
-          <button>Créer un contrat</button>
+          <button className='onecontact-btn'>Créer un contrat</button>
           {contact.contract_training !== undefined ? contact.contract_training.map(contract => {
-            let total = 0; // initialise la variable pour stocker la somme
             return (
               <div>
-                <div>
+                <div className='onecontact-cta'>
                     <h3>Contrat N°{contract.id}</h3>
-                    <button>Imprimer le contrat</button>
-                    <button>Modifier le contrat</button>
-                    <button>Supprimer le contrat</button>
+                    <button className='onecontact-btn'>Imprimer le contrat</button>
+                    <button className='onecontact-btn'>Modifier le contrat</button>
+                    <button className='onecontact-btn'>Supprimer le contrat</button>
                 </div>
-                <table style={{width: '100%'}}>
-                  <thead>
+                <table className='onecontact-table'>
+                  <thead className='onecontact-thead'>
                     <tr>
-                      <th style={{textAlign: 'start'}}>Date de début</th>
-                      <th style={{textAlign: 'end'}}>Date de fin</th>
-                      <th style={{textAlign: 'end'}}>Statut</th>
-                      <th style={{textAlign: 'end'}}>Action</th>
+                      <th className='onecontact-th' style={{textAlign: 'start'}}>Date de début</th>
+                      <th className='onecontact-th' style={{textAlign: 'end'}}>Date de fin</th>
+                      <th className='onecontact-th' style={{textAlign: 'end'}}>Statut</th>
+                      <th className='onecontact-th' style={{textAlign: 'end'}}>Action</th>
                     </tr>
                   </thead>
-                  <tbody>
-                    <tr>
-                      <td style={{textAlign: 'start'}}>{contract.start}</td>
-                      <td style={{textAlign: 'end'}}>{contract.end}</td>
-                      <td style={{textAlign: 'end'}}>{contract.status}</td>
-                      <td style={{textAlign: 'end'}}><a href="#">Details</a></td>
+                  <tbody className='onecontact-tbody'>
+                    <tr className='onecontact-active'>
+                      <td className='onecontact-td' style={{textAlign: 'start'}}>{contract.start}</td>
+                      <td className='onecontact-td' style={{textAlign: 'end'}}>{contract.end}</td>
+                      <td className='onecontact-td' style={{textAlign: 'end'}}>{contract.status}</td>
+                      <td className='onecontact-td' style={{textAlign: 'end'}}><a href="#">Details</a></td>
                     </tr>
                   </tbody>
                 </table>
@@ -229,29 +237,30 @@ function OneContactPage() {
           }) : 'loading...'}
         </fieldset>
 
+        <h3 className='onecontact-legend'>Prélèvement</h3>
+
         <fieldset className='onecontact-field'>
-          <legend className='onecontact-legend'>Prélèvement</legend>
-          <button>Ajouter un prélèvement</button>
-          <table style={{width: '100%'}}>
-            <thead>
+          <button className='onecontact-btn'>Ajouter un prélèvement</button>
+          <table className='onecontact-table'>
+            <thead className='onecontact-thead'>
               <tr>
-                <th style={{textAlign: 'start'}}>Date</th>
-                <th style={{textAlign: 'end'}}>Montant</th>
-                <th style={{textAlign: 'end'}}>Référence</th>
-                <th style={{textAlign: 'end'}}>Statut</th>
-                <th style={{textAlign: 'end'}}>Action</th>
+                <th className='onecontact-th' style={{textAlign: 'start'}}>Date</th>
+                <th className='onecontact-th' style={{textAlign: 'end'}}>Montant</th>
+                <th className='onecontact-th' style={{textAlign: 'end'}}>Référence</th>
+                <th className='onecontact-th' style={{textAlign: 'end'}}>Statut</th>
+                <th className='onecontact-th' style={{textAlign: 'end'}}>Action</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className='onecontact-tbody'>
             {contact.levies !== undefined ? contact.levies.map(levy => {
                 return (
-                  <tr key={levy.id}>
+                  <tr className='onecontact-active' key={levy.id}>
                     <>
-                      <td style={{textAlign: 'start'}}>{levy.date}</td>
-                      <td style={{textAlign: 'end'}}>{levy.amount}€</td>
-                      <td style={{textAlign: 'end'}}>{levy.reference}</td>
-                      <td style={{textAlign: 'end'}}>{levy.status}</td>
-                      <td style={{textAlign: 'end'}}> <a href="#">Details</a> </td>
+                      <td className='onecontact-td' style={{textAlign: 'start'}}>{levy.date}</td>
+                      <td className='onecontact-td' style={{textAlign: 'end'}}>{levy.amount}€</td>
+                      <td className='onecontact-td' style={{textAlign: 'end'}}>{levy.reference}</td>
+                      <td className='onecontact-td' style={{textAlign: 'end'}}>{levy.status}</td>
+                      <td className='onecontact-td' style={{textAlign: 'end'}}> <a href="#">Details</a> </td>
 
                     </>
                   </tr>
